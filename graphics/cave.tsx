44e6649f62f7ecd6c624eb0a5046582a111c3cf5
Mouthanos <thomasbioret.0905@gmail.com>
2022-11-23 17:03:49 +0100

    sorted tileset, map updated

--- graphics/cave.tsx
+++ graphics/cave.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.03.23" name="cave" tilewidth="16" tileheight="16" tilecount="336" columns="16">
- <image source="cave.png" width="256" height="336"/>
+<tileset version="1.9" tiledversion="1.9.2" name="cave" tilewidth="16" tileheight="16" tilecount="378" columns="18">
+ <image source="cave.png" width="288" height="240"/>
 </tileset>
