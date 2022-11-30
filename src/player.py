9bd3e1ee5c587d8b24ff728303943516ccc6412f
Mouthanos <thomasbioret.0905@gmail.com>
2022-11-30 09:44:15 +0100

    message

--- src/player.py
+++ src/player.py
@@ -1,24 +1,23 @@
 import pygame
 import math
-from map_manager import DynamicImage
+from utils import Hitbox
 
 class Player:
     def __init__(self):
         self.pos = pygame.Vector2(0, 0)
         self.direction = pygame.Vector2(0, 0)
         self.speed = 10
-        image = pygame.image.load("../graphics/player.png").convert_alpha()
-        self.image = DynamicImage(image)
 
     def get_rect(self):
         return pygame.Rect(self.pos.x, self.pos.y, 1, 1)
 
     def get_hitbox(self):
-        return pygame.Rect(self.pos.x, self.pos.y, 1,1)
+        return Hitbox(self.pos.x, self.pos.y, 1, 1)
 
     def move(self, map_manager, dt: float):
         if self.direction.magnitude() != 0:
             collisions_tiles = map_manager.get_around_collisions("map",self.pos, 1, ["collisions"])["collisions"]
+            #print(self.pos, collisions_tiles)
             velocity = self.direction.normalize() * self.speed * dt
             #self.pos += velocity
             
@@ -26,28 +25,35 @@ class Player:
             for i in range(step):
                 self.pos.x += velocity.x / step
                 self.collisions("x", velocity, collisions_tiles)
-            for i in range(step):
                 self.pos.y += velocity.y / step
                 self.collisions("y", velocity, collisions_tiles)
                 
     def collisions(self, direction: str, velocity: float, collisions: list) -> pygame.Vector2:
+        player_hitbox = self.get_hitbox()
         for tile_pos in collisions:
-            tile_rect = pygame.Rect(tile_pos[0], tile_pos[1], 1,1)
+            tile_hitbox = Hitbox(tile_pos[0], tile_pos[1], 1, 1)
             
-            if tile_rect.colliderect(self.get_hitbox()):
+            if tile_hitbox.overlap(player_hitbox):
                 #print("collisions", tile_rect)
+                print("-"*10)
+                print(player_hitbox)
+                print(tile_hitbox)
+                
                 if direction == "y":
                     if velocity.y > 0: # moving down
-                        self.pos.y = tile_rect.y - 1
+                        self.pos.y = tile_hitbox.y - player_hitbox.height
                     elif velocity.y < 0: # moving up
-                        self.pos.y = tile_rect.y + 1
+                        self.pos.y = tile_hitbox.y + tile_hitbox.height
                         
                 elif direction == "x":   
                     if velocity.x > 0: # moving right
-                        self.pos.x = tile_rect.x - 1
+                        self.pos.x = tile_hitbox.x - player_hitbox.width
 
                     elif velocity.x < 0: # moving left
-                        self.pos.x = tile_rect.x + 1
+                        self.pos.x = tile_hitbox.x + tile_hitbox.width
+
+                player_hitbox = self.get_hitbox()
+                print(player_hitbox)
             
 
     def event_handler(self, event: pygame.event):
@@ -69,4 +75,3 @@ class Player:
                 self.direction.y += 1
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 self.direction.y -= 1
-            
