242e8cc430f3356dcd5d32d0d4dc6b62ab1d53bc
Thomas BIORET <thomas.bioret@lyc-leonard-de-vinci-amboise.ac-orleans-tours.fr>
2022-12-16 14:46:49 +0100

    stats menu

--- src/display/ingame_menu.py
+++ src/display/ingame_menu.py
@@ -28,13 +28,14 @@ class Ingame_menu(UI):
     def main_display(self):
         self.clear_widget()
         self.set_background_color(None)
+        self.set_grab(False)
 
         self.fps_label  = Label(pygame.Vector2(10, 10), "", Font(15), pygame.Color(255, 255, 255))
         
         button_rect = pygame.Rect((0,20,100,40))
         button_rect.x = self.draw_surface.get_width() - button_rect.width - 20
         
-        self.button_menu = Button(button_rect, "Menu", Font(20), callback=self.main_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
+        button_menu = Button(button_rect, "Menu", Font(20), callback=self.main_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
         
         progressbar_rect = pygame.Rect((0, 0, 0, 0))
         progressbar_rect.x = 10
@@ -44,34 +45,38 @@ class Ingame_menu(UI):
 
         self.fight_bar = Progressbar(progressbar_rect, 0, 100, pygame.Color(85, 160, 39), pygame.Color(134, 221, 81))
 
-        self.bind_widget(self.fps_label)
-        self.bind_widget(self.button_menu)
-        self.bind_widget(self.fight_bar)
+        self.bind_several_widget(
+            self.fps_label,
+            button_menu,
+            self.fight_bar
+        )
     
     def main_menu(self):
         self.clear_widget()
         self.set_background_color(pygame.Color(20,20,20,150))
+        self.set_grab(True)
         
-        self.previous_button = Previous_button(self.draw_surface,self.main_display)
-        self.close_button = Close_button(self.draw_surface,self.main_display)
-        self.bind_widget(self.close_button)
-        self.bind_widget(self.previous_button)
-
+        previous_button = Previous_button(self.draw_surface,self.main_display)
+        close_button = Close_button(self.draw_surface,self.main_display)
 
         stats_button_rect = pygame.Rect(0,200,130,40)
         stats_button_rect.x = self.draw_surface.get_width()/2 - stats_button_rect.width/2
-        self.stats_button = Button(stats_button_rect,"Stats",Font(20),callback=self.stats_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
+        stats_button = Button(stats_button_rect,"Stats",Font(20),callback=self.stats_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
 
         equipment_button_rect = pygame.Rect(0,300,200,40)
         equipment_button_rect.x = self.draw_surface.get_width()/2 - equipment_button_rect.width/2
-        self.equipment_button = Button(equipment_button_rect,"Equipment",Font(20),callback=self.equipment_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
-        
-        self.bind_widget(self.stats_button)
-        self.bind_widget(self.equipment_button)
+        equipment_button = Button(equipment_button_rect,"Equipment",Font(20),callback=self.equipment_menu, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
 
         menu_label = Label(pygame.Vector2(40,40),"MENU",Font(30),pygame.Color(255,255,255))
-        self.bind_widget(menu_label)
-    
+       
+        self.bind_several_widget(
+            menu_label,
+            stats_button,
+            equipment_button,
+            close_button,
+            previous_button
+        )
+
     def stats_menu(self):
         self.clear_widget()
 
@@ -88,37 +93,76 @@ class Ingame_menu(UI):
 
         stats_title = Label(pygame.Vector2(40,40),"STATS",Font(30),pygame.Color(255,255,255))
 
-        self.bind_several_widget(close_button,previous_button,stats_title)
+        self.bind_several_widget(
+            close_button,
+            previous_button,
+            stats_title
+        )
 
-        value = 1000
+        self.value = {}
+        self.to_add_value = {}
+        self.points = 100
+        self.stats_labels = {}
         y = 140
         for stat in stats:
+            self.value[stat] = 1000
+            self.to_add_value[stat] = 0
+
             stats_label = Label(pygame.Vector2(70,y+10),stat,Font(20),pygame.Color(255,255,255))
+            
+            plus_button = Button(pygame.Rect(500,y,40,40),"+", Font(20), callback=lambda:self.add_point(stat), text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
+            minus_button = Button(pygame.Rect(650,y,40,40),"-", Font(20),callback=lambda:self.remove_point(stat), text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
 
-            plus_button = Button(pygame.Rect(500,y,40,40),"+", Font(20), callback=self.do_sth, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
-            minus_button = Button(pygame.Rect(650,y,40,40),"-", Font(20),callback=self.do_sth, text_color=pygame.Color(255, 255, 255), color=pygame.Color(120, 120, 120),  hover_color= pygame.Color(70, 70, 70))
+            stats_value_label = Label(pygame.Vector2(300,y+10),str(self.value[stat]),Font(20),pygame.Color(255,255,255))
+            to_add_value_label = Label(pygame.Vector2(565,y+10),str(self.to_add_value[stat]),Font(20),pygame.Color(255,255,255))
 
-            stats_value = Label(pygame.Vector2(300,y+10),str(value),Font(20),pygame.Color(255,255,255))
-            to_add_value = Label(pygame.Vector2(565,y+10),"100",Font(20),pygame.Color(255,255,255))
+            self.stats_labels[stat] = (stats_value_label,to_add_value_label)
 
-            self.bind_several_widget(stats_label,plus_button,minus_button,stats_value,to_add_value)
+            self.bind_several_widget(
+                stats_label,
+                plus_button,
+                minus_button,
+                stats_value_label,
+                to_add_value_label
+            )
 
             y += 70
 
     def equipment_menu(self):
         self.clear_widget()
         
-        self.previous_button = Previous_button(self.draw_surface,self.main_menu)
-        self.close_button = Close_button(self.draw_surface,self.main_display)
+        previous_button = Previous_button(self.draw_surface,self.main_menu)
+        close_button = Close_button(self.draw_surface,self.main_display)
 
-        self.bind_widget(self.close_button)
-        self.bind_widget(self.previous_button)
 
         equipment_label = Label(pygame.Vector2(40,40),"EQUIPMENT",Font(30),pygame.Color(255,255,255))
-        self.bind_widget(equipment_label)
+        
+        self.bind_several_widget(
+            equipment_label,
+            close_button,
+            previous_button
+        )
 
-    def do_sth(self):
-        pass
+    def add_point(self,stat):
+        print(stat)
+        if self.points != 0:
+            self.to_add_value[stat] += 1
+            self.points -= 1
+
+            self.stats_labels[stat][1].update_text(str(self.to_add_value[stat]))
+            self.stats_labels[stat][0].update_text(str(self.value[stat] + self.to_add_value[stat]))
+
+    def remove_point(self,stat):
+        if self.to_add_value[stat] != 0:
+            self.points += 1
+            self.to_add_value[stat] -= 1
+
+            self.stats_labels[stat][1].update_text(str(self.to_add_value[stat]))
+            self.stats_labels[stat][0].update_text(str(self.value[stat] + self.to_add_value[stat]))
+
+
+    def do_nothing(self):
+        return
 
 
 
