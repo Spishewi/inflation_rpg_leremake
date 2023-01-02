41c42174d1dacf3f4210614185996a5e853457ab
spishewi <spishewi@gmail.com>
2023-01-02 16:13:21 +0100

    ajout levels sur la map

---
+++ graphics/tileset_lvl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tileset_lvl" tilewidth="16" tileheight="16" tilecount="128" columns="4">
+ <image source="tileset_lvl.png" width="64" height="512"/>
+</tileset>
