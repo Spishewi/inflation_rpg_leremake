2cabefe7d79e8a5618405de70b36267ee035f286
Mouthanos <thomasbioret.0905@gmail.com>
2023-01-08 11:15:22 +0100

    comments

--- src/engine/player.py
+++ src/engine/player.py
@@ -9,31 +9,33 @@ class Player:
         self.pos = pygame.Vector2(0, 0)
         self.direction = pygame.Vector2(0, 0)
         self.speed = 10
-        #self.image = DynamicImage(pygame.image.load("../graphics/player.png"))
         self.hitbox_decalage = 0.6
         self.animation  = Animation("../graphics/player_keyframes")
     
-    def get_animation(self, factor):
+    def get_animation(self, factor): # Renvoie l'annimation du joueur en fonction de sa direction
         return self.animation.get_curentAnimation(self.direction, factor)
 
-    def get_rect(self):
+    def get_rect(self): # Revoie le rectangle dans lequel le joueur sera affiché 
         return pygame.Rect(self.pos.x, self.pos.y, 1, 1)
 
-    def get_hitbox(self):
+    def get_hitbox(self): # Renvoie la hitbox du joueur
         return Hitbox(self.pos.x, self.pos.y+self.hitbox_decalage, 1, 0.5)
 
     def move(self, map_manager, dt: float):
         if self.direction.magnitude() != 0:
+            # On récupère les tuiles aux alentours du joueur qui doivent avoir une collision
             collisions_tiles = map_manager.get_around_collisions("map",self.pos, 2, ["collisions"])["collisions"]
-            #print(self.pos, collisions_tiles)
             velocity = self.direction.normalize() * self.speed * dt
-            #self.pos += velocity
             
             step = max(math.ceil(abs(velocity.y)), math.ceil(abs(velocity.x)))
             for i in range(step):
+                # On fait avancer le joueur en x
                 self.pos.x += velocity.x / step
+                # On corrige son x si il y a une collision
                 self.collisions("x", velocity, collisions_tiles)
+                # On fait avancer le joueur en y
                 self.pos.y += velocity.y / step
+                # On corrige son y si il y a une collision
                 self.collisions("y", velocity, collisions_tiles)
                 
     def collisions(self, direction: str, velocity: float, collisions: list) -> pygame.Vector2:
@@ -42,30 +44,31 @@ class Player:
             collision : liste des tiles susceptibles d'être en collision avec le joueur
         """
         player_hitbox = self.get_hitbox()
-        for tile_pos in collisions:
+        for tile_pos in collisions: # On regarde pour toutes les tuiles aux alentours du joueur qui doivent avoir une collision
+            # On récupère la hitbox de la tuile
             tile_hitbox = Hitbox(tile_pos[0], tile_pos[1], 1, 1)
             
-            if tile_hitbox.overlap2(player_hitbox): # si il y a une collision
-                #print("collisions", tile_rect)
+            if tile_hitbox.overlap2(player_hitbox): # Si il y a une collision
                 
                 if direction == "y":
-                    if velocity.y > 0: # moving down
+                    if velocity.y > 0: # Moving down
                         self.pos.y = tile_hitbox.y - player_hitbox.height - self.hitbox_decalage
-                    elif velocity.y < 0: # moving up
+                    elif velocity.y < 0: # Moving up
                         self.pos.y = tile_hitbox.y + tile_hitbox.height - self.hitbox_decalage
                         
                 elif direction == "x":   
-                    if velocity.x > 0: # moving right
+                    if velocity.x > 0: # Moving right
                         self.pos.x = tile_hitbox.x - player_hitbox.width
 
-                    elif velocity.x < 0: # moving left
+                    elif velocity.x < 0: # Moving left
                         self.pos.x = tile_hitbox.x + player_hitbox.width
 
+                # On récupère la nouvelle hitbox du joueur
                 player_hitbox = self.get_hitbox()
 
     def event_handler(self, event: pygame.event): 
         """
-            defini la direction du joueur en fonction des touches pressés
+            définit la direction du joueur en fonction des touches pressées
         """
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_q:
@@ -86,7 +89,7 @@ class Player:
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 self.direction.y -= 1
         
-        # pour corriger des pb quand des inputs sont sauté,
+        # Pour corriger des pb quand des inputs sont sauté,
         # on réinitialise au cas où les inputs quand on touche pas au clavier
         key_sequence = pygame.key.get_pressed()
         if (not key_sequence[pygame.K_LEFT] and
